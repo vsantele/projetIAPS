3bb5b5c18da6ed84b59e448fe23eaf002f82b368
Thibaut <thibaut.berg@hotmail.com>
2023-04-15 16:53:33 +0200

    Add teams table

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,6 +7,27 @@ import ChatMessage from './models/ChatMessage'
 import React, {useEffect, useRef} from 'react'
 import { MessageAuthor } from './models/MessageAuthor'
 import Chat from "./components/Chat";
+import {DataGrid, GridColDef, GridRenderCellParams} from "@mui/x-data-grid";
+
+enum Team {
+  ITALY, NETHERLANDS, BELGIUM, GERMANY
+}
+
+const teams = [
+  {id: Team.ITALY, name: "Italie", cards: [1, 2, 4, 8, 12]},
+  {id: Team.NETHERLANDS, name: "Pays-Bas", cards: [5, 7, 10, 11, 12]},
+  {id: Team.BELGIUM, name: "Belgique", cards: [3, 5, 7, 9, 12]},
+  {id: Team.GERMANY, name: "Allemange", cards: [1, 6, 7, 8, 10]},
+];
+
+const teamsGridColumns: GridColDef[] = [
+  {headerName: "Équipe", field: "name", resizable: false, flex: 1},
+  {
+    headerName: "Cartes", field: "cards", resizable: false, flex: 3, renderCell: (params : GridRenderCellParams<number[]>) => (
+        <p>{params.value.join(" - ")}</p>
+    ),
+  },
+];
 
 function getAsciiValues(text: String) {
   const asciiCodes = []
@@ -103,7 +124,7 @@ function App() {
 
       <Grid container justifyContent="center" sx={{mt:5}}>
         <Grid container xs={5} justifyContent="center" alignItems="center">
-          <p>ToDo : Add teams table</p>
+          <DataGrid columns={teamsGridColumns} rows={teams} rowSelection={false} disableRowSelectionOnClick hideFooter={true} hideFooterPagination={true} hideFooterSelectedRowCount={true}/>
         </Grid>
 
         <Grid container xs={5}>
