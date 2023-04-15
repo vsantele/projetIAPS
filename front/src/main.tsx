9534d2db5cddd245b5c12df27b44f53c6ba392aa
Victor Santele <victor.santele@gmail.com>
2023-04-15 20:07:44 +0200

    Add cssbaseline from mui

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import '@fontsource/roboto/400.css'
+import { CssBaseline } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
