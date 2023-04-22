bb17f0b0926bf716f84a6748e11992a9e436234e
Victor Santele <victor.santele@gmail.com>
2023-04-22 18:19:30 +0200

    Update format cases

--- back/rules.pl
+++ back/rules.pl
@@ -3,10 +3,52 @@
 
 % Format: [Numéro * 10 + (A=1 , B=2 , C=3, D=4, 0), Position par rapport au numéro (numéro = 1, puis 2, etc)]
 % Si la route est coupé par des arbres, l'arbre est considéré comme une position. Exemple case 23, les positions valident sont 1, 2 et 4. 3 étant un arbres.
+caseEchange([130,2]).
+caseEchange([140,2]).
+caseEchange([180,2]).
+caseEchange([200,2]).
+caseEchange([200,3]).
+caseEchange([220,3]).
+caseEchange([260,4]).
+caseEchange([274, 4]).
+caseEchange([274,4]).
+caseEchange([300,4]).
+caseEchange([320,4]).
+caseEchange([360,2]).
+caseEchange([460,1]).
+caseEchange([460,2]).
+caseEchange([610,2]).
+caseEchange([620, 2]).
+caseEchange([710,2]).
+caseEchange([720,2]).
+caseEchange([810,1]).
+caseEchange([810,2]).
+caseEchange([850,1]).
+caseEchange([850,3]).
+caseEchange([860,3]).
+caseEchange([892, 2]).
+caseEchange([902,3]).
 
-casesEchange([[130,2] ,[140,2], [180,2], [200,2], [200,3], [220,3], [260,4], [274, 4], [274,4], [300,4], [320,4], [360,2], [460,1], [460,2], [610,2], [620, 2], [710,2], [720,2], [810,1], [810,2], [850,1], [850,3], [860,3], [892, 2], [902,3]]).
-
-casesChances([[91,1], [101,1], [110,1], [120,1], [150,2], [160,2], [190,3], [210,3], [240,1], [261, 1], [280,1], [300,1], [320,1], [340,1], [480,2], [570,2], [660,1], [660,2], [740,1], [903,3]]).
+caseChance([91,1]).
+caseChance([101,1]).
+caseChance([110,1]).
+caseChance([120,1]).
+caseChance([150,2]).
+caseChance([160,2]).
+caseChance([190,3]).
+caseChance([210,3]).
+caseChance([240,1]).
+caseChance([261,1]).
+caseChance([280,1]).
+caseChance([300,1]).
+caseChance([320,1]).
+caseChance([340,1]).
+caseChance([480,2]).
+caseChance([570,2]).
+caseChance([660,1]).
+caseChance([660,2]).
+caseChance([740,1]).
+caseChance([903,3]).
 
 sprint(1, 280, [1,1], [0,0]).
 sprint(2, 360, [4,3], [1,1]).
