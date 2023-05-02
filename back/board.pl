e08f31f2163da881b104ad9f462ba79aca7fc76c
Victor Santele <victor.santele@gmail.com>
2023-05-02 10:16:18 +0200

    Remove warning

--- back/board.pl
+++ back/board.pl
@@ -1,5 +1,8 @@
 :- module(board, [chemin/4,voisin/4 ]).
 % chemin(X1, Y1, X2, Y2) où X est le numéro de la case * 10 + (A=1,B=2,C=3) et Y la position par rapport à la case numéro. Exemple: 10, 1, 20, 1 signifie que la case 1 à la position 1 est reliée à la case 2 position 2.
+
+:- discontiguous chemin/4, voisin/4.
+
 chemin(10, 1, 20, 1).
 chemin(10, 1, 20, 2).
 voisin(10, 1, 10, 2).
